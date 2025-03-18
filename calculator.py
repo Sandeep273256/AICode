801947ee934021556c2e2e05cac3179863556564
sandeep273256 <sandeepkaurna28@gmail.com>
2025-03-18 14:17:00 +0200

    ok

---
+++ calculator.py
@@ -0,0 +1,16 @@
+
+num1 = float(input("Enter the first number: "))
+operation = input("Enter an operation (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+
+print("Result:", result)
